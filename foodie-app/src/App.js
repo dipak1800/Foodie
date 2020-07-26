eebe8e65c1d9b65c8adf8e26a758b66eca52edd6
Dipak Roy <59052983+dipak1800@users.noreply.github.com>
2020-07-26 23:07:05 +0530

    added some animation effect

--- foodie-app/src/App.js
+++ foodie-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Component/Navbar/Navbar";
+import Homepage from "./Pages/Homepage/Homepage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Homepage />
     </div>
   );
 }
--- foodie-app/src/Pages/Homepage/Homepage.jsx
+++ foodie-app/src/Pages/Homepage/Homepage.jsx
@@ -1,8 +1,43 @@
 import React from "react";
 import Style from "./Homepage.module.scss";
-import Pic from "../../Assets/Images/coffee.jpg";
+import Pic from "../../Assets/Images/food.jpg";
+import Typical from "react-typical";
 function Homepage() {
-  return <div style={Style.maincontainer} sty></div>;
+  return (
+    <div className={Style.maincontainer}>
+      <div className={Style.advertisement}>
+        <Typical
+          loop={Infinity}
+          wrapper="h2"
+          steps={[
+            "Welcome to _Food4Foodie_😍",
+            3000,
+            " Hungry ✋???",
+            2000,
+            "UnExpected Guests 👪???",
+            2000,
+            "Cooking Gone Wrong 🍚???",
+            2000,
+            "Party Night 🎉???",
+            2000,
+            "Late Night at Office 🏤???",
+            2000,
+            "Watching Movie 🎥???",
+            2000,
+            "Need Diet Food 💪 ???",
+            2000,
+            "Want to Eat Chinese 🍲???",
+            2000,
+            " Craving something 🌭???",
+            2000,
+          ]}
+        />
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+    </div>
+  );
 }
 
 export default Homepage;
